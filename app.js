b54c00caee184e91233ec68bce552748de8dca87
Matheo <matheo@wazoom-studio.com>
2022-05-10 18:03:02 -0400

    Optimization and security (150m)

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-const Thing = require('./models/Thing');
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://admin:admin@mongodb.etidj.mongodb.net/mongodb?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+  .then(() => console.log('Connexion to MongoDB succeed!'))
+  .catch(() => console.log('Connexion to MongoDB failed.'));
 
 app.use(express.json());
 
@@ -21,42 +22,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// POST routes
-app.post('/api/stuff', (req, res, next) => {
-  delete req.body._id;
-  const thing = new Thing({
-    ...req.body
-  });
-  thing.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
-    .catch(error => res.status(400).json({ error }));
-});
-
-// PUT routes
-app.put('/api/stuff/:id', (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(201).json({ message: 'Objet modifié !' }))
-    .catch(error => res.status(400).json({ error }));
-});
-
-// DELETE routes
-app.delete('/api/stuff/:id', (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(201).json({ message: 'Objet supprimé !' }))
-    .catch(error => res.status(400).json({ error }));
-});
-
-// GET routes
-app.get('/api/stuff/:id', (req, res, next) => {
-  Thing.findOne({ _id: req.params.id })
-    .then(thing => res.status(200).json(thing))
-    .catch(error => res.status(404).json({ error }));
-});
-
-app.get('/api/stuff', (req, res, next) => {
-  Thing.find()
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({ error }));
-});
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
---
+++ routes/stuff.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const stuffCtrl = require('../controllers/stuff');
+const auth = require('../middleware/auth');
+
+router.get('/', auth, stuffCtrl.getAllThings);
+router.get('/:id', auth, stuffCtrl.getOneThing);
+router.post('/', auth, stuffCtrl.createThing);
+router.put('/:id', auth, stuffCtrl.modifyThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
+
+module.exports = router;
